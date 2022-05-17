fc09030a0721361ae5395fd05abd81b3bd93cf6b
Essoh Jean Penuel <penuel225@gmail.com>
2022-05-17 09:54:53 +0000

    Mar 17, 09h 55

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+let boxLogin = document.querySelector('.box-login');
+let boxConnexion = document.querySelector('.connexion');
+
+boxLogin.addEventListener('mouseover', () => {
+    boxConnexion.classList.add('afficheConnexion');
+})
+
+boxLogin.addEventListener('mouseout', () => {
+    boxConnexion.classList.remove('afficheConnexion');
+})
+
+boxConnexion.addEventListener('mouseover', () => {
+    boxConnexion.classList.add('afficheConnexion');
+})
+boxConnexion.addEventListener('mouseout', () => {
+    boxConnexion.classList.remove('afficheConnexion');
+})
