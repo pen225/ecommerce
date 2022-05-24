d40c84838fa496898c56a9bf3e8f74fbd773f09e
Essoh Jean Penuel <penuel225@gmail.com>
2022-05-24 01:29:05 +0000

    01H28

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-let boxLogin = document.querySelector('.box-login');
+let boxLogin = document.querySelector('.login');
 let boxConnexion = document.querySelector('.connexion');
 
 boxLogin.addEventListener('mouseover', () => {
@@ -16,3 +16,32 @@ boxConnexion.addEventListener('mouseout', () => {
     boxConnexion.classList.remove('afficheConnexion');
 })
 
+// Add quantite panier
+
+let btnAddPanier = document.querySelector('.add-panier');
+let btnSoustracPanier = document.querySelector('.souatract-panier');
+let resultQtePanier = document.querySelector('.result-qte-panier');
+let nombreArticle = document.querySelector('.nombre-article');
+let prixLivraison = document.querySelector('.prix-livraison');
+let totalPanier = document.querySelector('.total-panier');
+let prxArticle = document.querySelector('.prix-article');
+let totalPan
+
+totalPanier.textContent = ((+prxArticle.textContent) * (+nombreArticle.textContent)) + parseInt(prixLivraison.textContent)
+
+
+
+btnAddPanier.addEventListener('click', () =>{
+    resultQtePanier.value = (+resultQtePanier.value) + 1;
+    nombreArticle.textContent = resultQtePanier.value;
+    totalPan = ((+prxArticle.textContent) * (+nombreArticle.textContent)) + parseInt(prixLivraison.textContent)
+    totalPanier.textContent = totalPan;
+})
+btnSoustracPanier.addEventListener('click', () =>{
+    if (resultQtePanier.value > 1) {
+        resultQtePanier.value = (+resultQtePanier.value) - 1;
+        nombreArticle.textContent = resultQtePanier.value;
+        totalPan = ((+prxArticle.textContent) * (+nombreArticle.textContent)) + parseInt(prixLivraison.textContent)
+        totalPanier.textContent = totalPan;
+    }
+})
